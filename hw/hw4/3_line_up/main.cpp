0daa5e6ad34ca4952b44fba84368824f8041e2e5
vic-lee <viclee@nyu.edu>
2020-02-19 22:38:23 -0500

    Implement GroupQueue class

--- hw/hw4/3_line_up/main.cpp
+++ hw/hw4/3_line_up/main.cpp
@@ -2,7 +2,8 @@
 #include <iostream>
 #include <map>
 #include <sstream>
-#include <vector>
+
+#define ELEM_UNDEF 1000000
 
 using namespace std;
 using IDtoGroupMap = map<unsigned int, unsigned int>;
@@ -24,7 +25,41 @@ struct Input {
     IDtoGroupMap id_group_map;
 };
 
-Input read_in() {
+class GroupQueue {
+   private:
+    IDtoGroupMap id_to_group_;
+    deque<deque<unsigned int>> gqueue_;
+    map<unsigned int, deque<unsigned int>*> group_refs_;
+
+   public:
+    GroupQueue(IDtoGroupMap id_group_map) : id_to_group_(id_group_map){};
+
+    void push(unsigned int x) {
+        unsigned int next_elem_group_id = id_to_group_[x];
+        if (group_refs_.find(next_elem_group_id) != group_refs_.end()) {
+            group_refs_[next_elem_group_id]->push_back(x);
+        } else {
+            gqueue_.push_back(deque<unsigned int>({x}));
+            group_refs_[next_elem_group_id] = &gqueue_.back();
+        }
+    }
+
+    unsigned int pop() {
+        if (!gqueue_.empty()) {
+            const auto pop_target = gqueue_.front().front();
+            if (gqueue_.front().size() == 1) {
+                group_refs_.erase(id_to_group_[pop_target]);
+                gqueue_.pop_front();
+            } else {
+                gqueue_.front().pop_front();
+            }
+            return pop_target;
+        }
+        return ELEM_UNDEF;
+    }
+};
+
+IDtoGroupMap read_in() {
     unsigned short group_cnt;
     unsigned int elem_cnt = 0;
     cin >> group_cnt;
@@ -45,14 +80,11 @@ Input read_in() {
         }
     }
 
-    return {group_cnt, elem_cnt, idmap};
+    return idmap;
 }
 
-ostringstream process_commands(IDtoGroupMap id_group_map,
-                               unsigned int group_cnt) {
-    auto gqueue = deque<deque<unsigned int>>();
-    auto group_refs = map<unsigned int, deque<unsigned int>*>();
-
+ostringstream process_commands(IDtoGroupMap id_group_map) {
+    auto gq = GroupQueue(id_group_map);
     string line, cmd;
     ostringstream oss;
 
@@ -60,26 +92,11 @@ ostringstream process_commands(IDtoGroupMap id_group_map,
         if (line == "Shutdown") {
             break;
         } else if (line == "Pop") {
-            if (!gqueue.empty()) {
-                const auto to_pop = gqueue.front().front();
-                oss << to_pop << '\n';
-                if (gqueue.front().size() == 1) {
-                    group_refs.erase(id_group_map[to_pop]);
-                    gqueue.pop_front();
-                } else {
-                    gqueue.front().pop_front();
-                }
-            }
-        } else {  // Push xx
-            unsigned int tgt;
-            stringstream(line) >> cmd >> tgt;
-            unsigned int tgt_group = id_group_map[tgt];
-            if (group_refs.find(tgt_group) != group_refs.end()) {
-                group_refs[tgt_group]->push_back(tgt);
-            } else {
-                gqueue.push_back(deque<unsigned int>({tgt}));
-                group_refs[tgt_group] = &gqueue.back();
-            }
+            oss << gq.pop() << '\n';
+        } else {
+            unsigned int target;
+            stringstream(line) >> cmd >> target;
+            gq.push(target);
         }
     }
 
@@ -87,6 +104,6 @@ ostringstream process_commands(IDtoGroupMap id_group_map,
 }
 
 int main() {
-    auto in = read_in();
-    cout << process_commands(in.id_group_map, in.group_cnt).str();
+    auto id_group_map = read_in();
+    cout << process_commands(id_group_map).str();
 }
