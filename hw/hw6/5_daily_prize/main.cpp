f329ed43335f448a0a88cdcbd3c8470c3e9709fe
vic-lee <viclee@nyu.edu>
2020-03-02 11:35:57 -0500

    Daily Prize initial submission
    Failed 4/99 test cases

---
+++ hw/hw6/5_daily_prize/main.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <queue>
+#include <sstream>
+#include <vector>
+
+using namespace std;
+using ULL = unsigned long long;
+using Receipts = vector<vector<unsigned int>>;
+using MaxHeap = priority_queue<unsigned int>;
+using MinHeap =
+    priority_queue<unsigned int, vector<unsigned int>, greater<unsigned int>>;
+
+Receipts read_in() {
+    unsigned int days_cnt, daily_receipt_cnt, x;
+    cin >> days_cnt;
+    Receipts receipts(days_cnt);
+    for (unsigned int day = 0; day < days_cnt; day++) {
+        cin >> daily_receipt_cnt;
+        for (unsigned int i = 0; i < daily_receipt_cnt; i++) {
+            cin >> x;
+            receipts[day].push_back(x);
+        }
+    }
+    return receipts;
+}
+
+ULL calc_prizes(Receipts& receipts) {
+    MinHeap minheap;
+    MaxHeap maxheap;
+    ostringstream oss;
+    ULL total = 0;
+
+    for (auto const& daily : receipts) {
+        for (auto const& rct : daily) {
+            minheap.push(rct);
+            maxheap.push(rct);
+        }
+        unsigned int min_rct = minheap.top();
+        unsigned int max_rct = maxheap.top();
+        minheap.pop();
+        maxheap.pop();
+        total += max_rct - min_rct;
+    }
+
+    return total;
+}
+
+int main() {
+    auto receipts = read_in();
+    cout << calc_prizes(receipts) << endl;
+}
