ad2bedba09b284924578245cfe78b28cebf24629
vic-lee <viclee@nyu.edu>
2020-03-07 17:41:22 -0500

    Remove radix sort

--- hw/hw7/1_grade_sort/main.cpp
+++ hw/hw7/1_grade_sort/main.cpp
@@ -30,40 +30,6 @@ vector<unsigned int> read_in() {
     return scores;
 }
 
-// void exp_counting_sort(vector<unsigned int>& V, unsigned int exp) {
-//     vector<unsigned int> counts(10, 0);
-//     vector<unsigned int> output(V.size(), 0);
-//     for (auto const& e : V)
-//         counts[(e / exp) % 10]++;
-
-//     // for (size_t i = 0; i < counts.size(); i++)
-//     //     cout << i << ": " << counts[i] << "  ";
-//     // cout << '\n';
-//     for (size_t i = 1; i < counts.size(); i++)
-//         counts[i] += counts[i - 1];
-//     // for (size_t i = 0; i < counts.size(); i++)
-//     //     cout << i << ": " << counts[i] << "  ";
-//     // cout << '\n';
-
-//     unsigned int key;
-//     for (int i = V.size() - 1; i >= 0; i--) {
-//         key = (V[i] / exp) % 10;
-//         // cout << "assigning " << V[i] << " to position " << counts[key] - 1
-//         //      << " (key: " << key << " " << (V[i] / exp) << " "
-//         //      << ((V[i] / exp) % 10) << " " << counts[key] << ")" << '\n';
-//         output[counts[key] - 1] = V[i];
-//         counts[key]--;
-//     }
-//     copy(output.begin(), output.end(), V.begin());
-// }
-
-// void radix_sort(vector<unsigned int>& V) {
-//     for (unsigned int exp = 1; MAX / exp > 0; exp *= 10) {
-//         // cout << "doing exp " << exp << '\n';
-//         exp_counting_sort(V, exp);
-//     }
-// }
-
 void counting_sort(vector<unsigned int>& V) {
     vector<unsigned int> counts(101, 0);
     for (auto const& e : V)
@@ -87,8 +53,6 @@ int main() {
     cin.tie(NULL);
 
     auto scores = read_in();
-    // sort(scores.begin(), scores.end());
-    // radix_sort(scores);
     counting_sort(scores);
     fmtout(scores);
 }
