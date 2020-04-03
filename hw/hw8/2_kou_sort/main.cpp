becf4c66b3c6432a5c9ee9ec2ecc1b52cf6f78fd
vic-lee <viclee@nyu.edu>
2020-04-02 20:02:06 -0400

    Optimize hw8/2_kou_sort performance
    Use statically allocated array instead.
    Fixes all the timeout cases.

--- hw/hw8/2_kou_sort/main.cpp
+++ hw/hw8/2_kou_sort/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <sstream>
-#include <vector>
+
+#define MAX_SZ 500000
 
 using namespace std;
 using UIN = unsigned int;
@@ -15,9 +16,9 @@ void print_iterable(T items) {
     cout << oss.str() << endl;
 }
 
-ULL _merge(vector<UIN> &nums, size_t L, size_t R, size_t M) {
+ULL _merge(int *nums, size_t L, size_t R, size_t M) {
     size_t i = L, j = M + 1, k = L;
-    vector<UIN> buf(nums.size(), 0);
+    int buf[MAX_SZ];
     ULL inv_cnt = 0;
 
     while (i <= M && j <= R) {
@@ -37,7 +38,7 @@ ULL _merge(vector<UIN> &nums, size_t L, size_t R, size_t M) {
     return inv_cnt;
 }
 
-ULL _mergesort(vector<UIN> &nums, size_t L, size_t R) {
+ULL _mergesort(int *nums, size_t L, size_t R) {
     ULL inv_cnt = 0;
     if (L < R) {
         int M = (L + R) / 2;
@@ -48,18 +49,17 @@ ULL _mergesort(vector<UIN> &nums, size_t L, size_t R) {
     return inv_cnt;
 }
 
-ULL count_inversions(vector<UIN> &nums) {
-    return _mergesort(nums, 0, nums.size() - 1);
-}
+ULL count_inversions(int *nums, int sz) { return _mergesort(nums, 0, sz - 1); }
 
 int main() {
     std::ios::sync_with_stdio(false);
     cin.tie(NULL);
 
+    int nums[MAX_SZ];
+
     UIN nums_sz;
     scanf("%u\n", &nums_sz);
-    vector<UIN> nums(nums_sz, 0);
     for (size_t i = 0; i < nums_sz; i++) scanf("%u\n", &nums[i]);
 
-    cout << count_inversions(nums) << endl;
+    cout << count_inversions(nums, nums_sz) << endl;
 }
