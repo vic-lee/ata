c358a3d788ac0b8fdd718aef40aa3863359cfa69
vic-lee <viclee@nyu.edu>
2020-05-16 23:02:16 -0400

    Complete rct10/A

--- rct/rct10/A/main.cpp
+++ rct/rct10/A/main.cpp
@@ -2,48 +2,113 @@
 #include <sstream>
 #include <vector>
 
+#define FOR_N(n) for (size_t i = 0; i < n; i++)
+
 using namespace std;
 using UIN = unsigned int;
+using LL  = long long;
 using ULL = unsigned long long;
 
 template <typename T>
 void print_iterable(const T& items) {
-    ostringstream oss;
+    std::ostringstream oss;
     for (auto const& item : items) {
         oss << item << '\n';
     }
-    cout << oss.str();
+    std::cout << oss.str();
 }
 
-template <typename InIt, typename Predicate, typename UnaryFunction>
-UnaryFunction for_each_if(InIt first, InIt last, UnaryFunction fn,
-                          Predicate pr) {
-    for (; first != last; ++first) {
-        if (pr(*first)) fn(*first);
+struct SailDirection {
+    bool should_sail_east;
+    bool should_sail_south;
+    bool should_sail_west;
+    bool should_sail_north;
+};
+
+struct Coor {
+    LL x;
+    LL y;
+
+    Coor(LL x, LL y) : x(x), y(y) {}
+
+    void move_east() { x++; }
+
+    void move_west() { x--; }
+
+    void move_north() { y++; }
+
+    void move_south() { y--; }
+
+    bool operator==(const Coor& other) const {
+        return x == other.x && y == other.y;
     }
-    return fn;
-}
 
-struct Input {};
+    SailDirection relative_to(const Coor& dest) const {
+        bool should_sail_east  = dest.x > x;
+        bool should_sail_west  = dest.x < x;
+        bool should_sail_north = dest.y > y;
+        bool should_sail_south = dest.y < y;
 
-Input read_in() {
-    Input  in;
-    int    t, s_x, s_y, e_x, e_y;
-    string wind_directions;
-    scanf("%d %d %d %d %d", &t, &s_x, &s_y, &e_x, &e_y);
-    getline(cin, wind_directions);
+        return {
+            should_sail_east,
+            should_sail_south,
+            should_sail_west,
+            should_sail_north,
+        };
+    }
+};
+
+struct ProcessedInput {
+    std::vector<char> wind_directions;
+    Coor              start;
+    Coor              end;
+    UIN               time_limit;
+
+    ProcessedInput(UIN time_limit, Coor start, Coor end)
+        : time_limit(time_limit), start(start), end(end) {
+        wind_directions = vector<char>(time_limit, ' ');
+    }
+};
+
+ProcessedInput read_in() {
+    UIN time_lim;
+    LL  s_x, s_y, e_x, e_y;
+
+    std::cin >> time_lim >> s_x >> s_y >> e_x >> e_y;
+    ProcessedInput in(time_lim, Coor(s_x, s_y), Coor(e_x, e_y));
+
+    FOR_N(in.time_limit) { std::cin >> in.wind_directions[i]; }
+
     return in;
 }
 
-void process(Input& in) {}
+int attempt_sail(ProcessedInput& in) {
+    for (size_t i = 0; i < in.wind_directions.size(); i++) {
+        SailDirection dir = in.start.relative_to(in.end);
 
-void fmtout() {}
+        const char curr_wind_dir = in.wind_directions[i];
+
+        if (dir.should_sail_east && curr_wind_dir == 'E') {
+            in.start.move_east();
+        } else if (dir.should_sail_west && curr_wind_dir == 'W') {
+            in.start.move_west();
+        } else if (dir.should_sail_south && curr_wind_dir == 'S') {
+            in.start.move_south();
+        } else if (dir.should_sail_north && curr_wind_dir == 'N') {
+            in.start.move_north();
+        }
+
+        if (in.start == in.end) {  // reached destination
+            return i + 1;
+        }
+    }
+    return -1;
+}
 
 int main() {
     std::ios::sync_with_stdio(false);
-    cin.tie(NULL);
+    std::cin.tie(NULL);
 
     auto in = read_in();
-    process(in);
-    fmtout();
+    std::cout << attempt_sail(in) << std::endl;
 }
