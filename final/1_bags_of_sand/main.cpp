54251437beac1e7a50cb790eaf30caac9c2c122d
vic-lee <viclee@nyu.edu>
2020-05-18 13:31:59 -0400

    Complete final Q1
    Score: 10/10

---
+++ final/1_bags_of_sand/main.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <queue>
+#include <sstream>
+#include <vector>
+
+using namespace std;
+using UIN = unsigned int;
+using ULL = unsigned long long;
+
+template <typename T>
+using MinQueue = priority_queue<T, vector<T>, greater<T>>;
+
+#define FOR_N(n) for (int i = 0; i < n; i++)
+
+template <typename T>
+void print_iterable(const T& items) {
+    ostringstream oss;
+    for (auto const& item : items) {
+        oss << item << '\n';
+    }
+    cout << oss.str();
+}
+
+MinQueue<UIN> read_in() {
+    UIN num_bags;
+    cin >> num_bags;
+    MinQueue<UIN> nums;
+
+    UIN bag;
+    FOR_N(num_bags) {
+        cin >> bag;
+        nums.push(bag);
+    }
+
+    return nums;
+}
+
+ULL min_bagging_cost(MinQueue<UIN>& bags) {
+    ULL cost = 0;
+
+    do {
+        auto bag1 = bags.top();
+        bags.pop();
+        auto bag2 = bags.top();
+        bags.pop();
+        auto new_bag_cost = bag1 + bag2;
+        cost += new_bag_cost;
+        bags.push(new_bag_cost);
+    } while (bags.size() >= 2);
+
+    return cost;
+}
+
+void fmtout() {}
+
+int main() {
+    std::ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    auto in = read_in();
+    cout << min_bagging_cost(in) << endl;
+}
