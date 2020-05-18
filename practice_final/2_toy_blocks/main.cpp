b599452bb143b0063b7f31b93f90c19f8c2a9efc
vic-lee <viclee@nyu.edu>
2020-05-17 20:55:43 -0400

    Update practice final Q2 solution
    Score: 6.1/10.0

--- practice_final/2_toy_blocks/main.cpp
+++ practice_final/2_toy_blocks/main.cpp
@@ -2,6 +2,7 @@
 #include <cmath>
 #include <iostream>
 #include <numeric>
+#include <set>
 #include <sstream>
 #include <vector>
 
@@ -27,65 +28,60 @@ std::vector<int> read_in() {
 
     FOR_N(block_count) cin >> blocks[i];
 
-    std::sort(blocks.begin(), blocks.end(), greater<int>());
-
     return blocks;
 }
 
-struct Tower {
-    ULL sum;
-    ULL height;
-
-    Tower() {
-        sum    = 0;
-        height = 0;
-    }
-
-    void add(int x) {
-        sum += x;
-        height++;
-    }
-};
-
-void construct_blocks(const vector<int>& rsorted_blocks) {
-    if (rsorted_blocks.size() == 1) {
-        std::cout << "0 " << rsorted_blocks[0] << std::endl;
+void construct_blocks(const vector<int>& blocks) {
+    if (blocks.size() == 1) {
+        std::cout << "0 " << blocks[0] << std::endl;
         return;
     }
 
-    if (rsorted_blocks.size() == 2) {
-        std::cout << rsorted_blocks[1] << " " << rsorted_blocks[0] << std::endl;
+    if (blocks.size() == 2) {
+        int block1 = blocks[0], block2 = blocks[1];
+        if (block1 > block2) swap(block1, block2);
+        std::cout << block1 << " " << block2 << std::endl;
     }
 
-    const ULL blocks_sum =
-        accumulate(rsorted_blocks.begin(), rsorted_blocks.end(), 0);
+    const ULL blocks_sum = accumulate(blocks.begin(), blocks.end(), 0);
 
-    Tower t1, t2;
-    t1.add(rsorted_blocks[0]);
-    t2.add(rsorted_blocks[1]);
+    std::set<size_t> tower_elems;
+    int              target   = blocks_sum / 2;
+    int              tower_sz = blocks.size() / 2;
+    int tower_sum = accumulate(blocks.begin(), blocks.begin() + tower_sz, 0);
 
-    for (size_t i = 2; i < rsorted_blocks.size(); i++) {
-        auto curr_block = rsorted_blocks[i];
-        if (t1.height == t2.height) {
-            if (t1.sum < t2.sum) {
-                t1.add(curr_block);
-            } else {
-                t2.add(curr_block);
+    for (size_t i = 0; i < tower_sz; i++) {
+        tower_elems.insert(i);
+    }
+
+    for (size_t i = tower_sz; i < blocks.size(); i++) {
+        int curr_delta        = abs(tower_sum - target);
+        int best_delta        = curr_delta;
+        int block_candidate   = blocks[i];
+        int removal_candidate = -1;
+
+        for (auto const elem : tower_elems) {
+            int this_delta =
+                abs(tower_sum - blocks[elem] + block_candidate - target);
+            if (this_delta < best_delta) {
+                removal_candidate = elem;
+                best_delta        = this_delta;
             }
-        } else if (t1.height < t2.height) {
-            t1.add(curr_block);
-        } else {  // t1.height > t2.height
-            t2.add(curr_block);
+        }
+
+        if (removal_candidate != -1) {
+            tower_elems.erase(removal_candidate);
+            tower_elems.insert(i);
+            tower_sum = 0;
+            for (auto const elem : tower_elems) {
+                tower_sum += blocks[elem];
+            }
         }
     }
 
-    if (t1.sum > t2.sum) {
-        Tower tmp = t1;
-        t1        = t2;
-        t2        = tmp;
-    }
-
-    std::cout << t1.sum << " " << t2.sum << std::endl;
+    int block1 = tower_sum, block2 = blocks_sum - tower_sum;
+    if (block1 > block2) swap(block1, block2);
+    std::cout << block1 << " " << block2 << std::endl;
 }
 
 int main() {
